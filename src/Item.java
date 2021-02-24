c60d744fb9d7ebb4b68d33a2d39e168c9f03ba35
Derek S S Wong <dsw6@hw.ac.uk>
2021-02-24 11:37:29 +0000

    Added set methods to Item class

--- src/Item.java
+++ src/Item.java
@@ -17,19 +17,29 @@ public class Item {
 		this.itemPrice = itemPrice;
 	}
 	
-	public void setItemID(String ID) { this.itemID = ID;}
+	//Set and get for itemID
+	public void setItemID(String itemID) { this.itemID = itemID;}
 	
 	public String getItemID() { return this.itemID; }
-
+	
+	//Set and get for itemCat
+	public void setItemCat(String itemCat) { this.itemCat = ItemCat.valueOf(itemCat); }
 	
 	public ItemCat getItemCat() { return this.itemCat; }
 	
-	public void setItemName(String ItemName){this.itemName = ItemName ; }
+	//Set and get for itemName
+	public void setItemName(String itemName){ this.itemName = itemName; }
 	
 	public String getItemName() { return this.itemName; }
 	
 	
+	//Set and get for itemDesc
+	public void setItemDesc(String itemDesc) { this.itemDesc = itemDesc; }
+	
 	public String getItemDesc() { return this.itemDesc; }
+	
+	//Set and get for itemPrice
+	public void setItemPrice(double itemPrice) { this.itemPrice = itemPrice; }
 	
 	public double getItemPrice() { return this.itemPrice; }
 	
