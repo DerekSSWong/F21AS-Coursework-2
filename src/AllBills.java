c70352d428e0c67b9d04dcc7bbc8aeef570f742c
djr2000 <djr2000@hw.ac.uk>
2021-03-03 17:39:57 +0000

    Added all bills and all orders

---
+++ src/AllBills.java
@@ -0,0 +1,52 @@
+import java.util.HashMap;
+
+/**
+ * 
+ * Class for storing all bills
+ * 
+ * @author Dan Ryan
+ *
+ */
+
+public class AllBills {
+	
+	HashMap<Integer, Bill> ab = new HashMap<Integer, Bill>();
+	
+	/**
+	 * adds bill to hashmap using bill customerID as the key
+	 * 
+	 * @param bill
+	 */
+	
+	public void addBill(Bill bill) {
+		ab.put(bill.getCustomerID(), bill);
+	}
+	
+	/**
+	 * Creates a net income
+	 * Loops through ab hashmap entries and adds the calculated price from each bill to the net income
+	 * 
+	 * @return double
+	 */
+	
+	public double getNetIncome() {
+		Double netIncome = 0.00;
+		for (HashMap.Entry<Integer, Bill> entry : ab.entrySet()) {
+			netIncome += entry.getValue().calculateTotalPrice();
+			
+		}//for
+		return netIncome;
+	}//getNetIncome
+	
+	/**
+	 * finds a bill using the customer ID as a key
+	 * 
+	 * @param cusID
+	 * @return Bill
+	 */
+	
+	public Bill findBill(int cusID) {
+		return ab.get(cusID);
+	} 
+	
+}
